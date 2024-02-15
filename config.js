93c07601dd355fa425ea8e0ae0fb39cfd669d173
kanele262 <135858254+kanele262@users.noreply.github.com>
2024-02-15 23:39:20 +0700

    Update config.js

--- config.js
+++ config.js
@@ -13,4 +13,5 @@ const CONFIG = {
     mess: 'Toy biết mà 🥰. Yêu Uyenn nhiều nhiều 😘😘',
     messDesc: 'Chúc chị pé năm mới ngày càng xênh xĩu <333',
     btnAccept: 'Ỏoooooo <3',
+    messLink: 'http://fb.com'
 }
